c1c8b785fef44d4d0886c82aaa9520c917a82e09
Cody Southworth <southworth.10@wright.edu>
2023-04-14 01:44:40 -0400

    Create OU-bulkcreate.ps1

---
+++ Projects/Project3/OU-bulkcreate.ps1
@@ -0,0 +1,19 @@
+$secpass = Read-Host "Enter Default Password for Accounts" -AsSecureString
+
+Import-Csv C:\Users\Administrator\Desktop\users.csv |
+foreach {
+    $ou1 = $_.OU1
+    $ou2 = $_.OU2
+    $ouPath = "OU=$ou2,OU=$ou1,DC=ad,DC=taiga,DC=org"
+    $userProps = @{
+        Name = $_.FirstName + "," + $_.LastName
+        GivenName = $_.FirstName
+        Surname = $_.LastName
+        SamAccountName = $_.SamAccountName
+        UserPrincipalName = "$($_.SamAccountName)@adtaiaga.com"
+        AccountPassword = $secpass
+        Path = $ouPath
+        Enabled = $true
+    }
+    New-ADUser @userProps
+}
