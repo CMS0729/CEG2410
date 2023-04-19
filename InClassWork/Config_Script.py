1f4eeeacd96a49571e46473caac156a17df46ec5
Cody Southworth <89545998+CMS0729@users.noreply.github.com>
2023-04-18 23:34:31 -0400

    Update Config_Script.py

--- InClassWork/Config_Script.py
+++ InClassWork/Config_Script.py
@@ -6,6 +6,9 @@ def install_packages():
     packages = ["git", "vim", "python3", "snort"]
     for package in packages:
         subprocess.run(["sudo", "apt-get", "install", "-y", package])
+        
+ def vim_customization():
+    
        
 ## Driver 
 if __name__ == "__main__":
