afa19874877fba1b415baf994c4c72b375ee4429
Cody Southworth <89545998+CMS0729@users.noreply.github.com>
2023-04-18 23:32:15 -0400

    Update Config_Script.py

--- InClassWork/Config_Script.py
+++ InClassWork/Config_Script.py
@@ -1,2 +1,10 @@
 import os
 import subprocess
+
+def install_packages():
+    packages = ["git", "vim", "python3", "snort"]
+    for package in packages:
+        subprocess.run(["sudo", "apt-get", "install", "-y", package])
+        
+if __name__ == "__main__":
+    install_packages()
