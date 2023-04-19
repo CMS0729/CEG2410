5ad9590b4f12f8b700418a5b2e30f8e52a132353
Cody Southworth <89545998+CMS0729@users.noreply.github.com>
2023-04-18 23:50:44 -0400

    Update Config_Script.py

--- InClassWork/Config_Script.py
+++ InClassWork/Config_Script.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 
-## Function that installs the desired packages
+# Function that installs the desired packages
 def install_packages():
     ## Variable packages that stores each package in a list.
     packages = ["git", "vim", "python3", "snort"]
@@ -9,13 +9,20 @@ def install_packages():
     for package in packages:
         subprocess.run(["sudo", "apt-get", "install", "-y", package])
  
-## Function that installs the desired vim customizations 
+# Function that installs the desired vim customizations 
 def vim_customization():
     # Change default colorization
     with open(os.path.expanduser("~/.vimrc"), "a") as f:
         f.write("syntax on\n")
+        
+# Add functionality
+    with open(os.path.expanduser("~/.vimrc"), "a") as f:
+        # Link to where I got idea: https://linuxhandbook.com/vim-indentation-tab-spaces/
+        f.write("set noexpandtab\n")
+        f.write("set tabstop=4\n")
+        f.write("set shiftwidth=4\n")
        
-## Driver 
+# Driver 
 if __name__ == "__main__":
     ## Calls the install_packages function
     install_packages()
