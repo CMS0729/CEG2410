8906146c976af733465b7a8dd41c96a6cfc07363
Cody Southworth <89545998+CMS0729@users.noreply.github.com>
2023-04-18 23:33:11 -0400

    Update Config_Script.py

--- InClassWork/Config_Script.py
+++ InClassWork/Config_Script.py
@@ -1,10 +1,12 @@
 import os
 import subprocess
 
+## Installs the desired packages
 def install_packages():
     packages = ["git", "vim", "python3", "snort"]
     for package in packages:
         subprocess.run(["sudo", "apt-get", "install", "-y", package])
-        
+       
+## Driver 
 if __name__ == "__main__":
     install_packages()
